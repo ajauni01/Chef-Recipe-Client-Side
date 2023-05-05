a7a5c25eae618982b0480945b0415e6568e50d5e
AJHARUL ISLAM AUNIK <AJAUNI01@WSC.EDU>
2023-05-05 12:20:05 -0500

    final-changes

--- src/Components/Main/Login/Login.jsx
+++ src/Components/Main/Login/Login.jsx
@@ -2,11 +2,17 @@ import React, { useContext, useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { AuthContext } from '../Providers/AuthProvider';
 import { Link, useNavigate } from 'react-router-dom';
+import { FaGoogle } from 'react-icons/fa';
+import { GoogleAuthProvider } from "firebase/auth";
+
+
 
 
 
 const Login = () => {
+  // navigate the user to the home page after a successful login
   const navigate = useNavigate();
+  const provider = new GoogleAuthProvider();
 
   // useState to handle error
   const [error, setError] = useState('');
@@ -43,28 +49,34 @@ const Login = () => {
 
   return (
 
-    <Form onSubmit={handleSubmit} className='text-center mt-4'>
-      <div className="mb-3">
+    <div>
+      <Form onSubmit={handleSubmit} className='text-center mt-4'>
+        <div className="mb-3">
 
-        {/* user email address */}
-        <label htmlFor="exampleInputEmail" className="form-label">Email address</label>
-        <input type="email" className="form-control text-center fw-bold fs-5" id="exampleInputEmail1" aria-describedby="emailHelp" name='email' required />
-        <div id="emailHelp" className="form-text ">We'll never share your email with anyone else.</div>
-      </div>
-      <div className="mb-3">
-        <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-        <input type="password" className="form-control text-center fw-bold fs-5" id="exampleInputPassword1" name='password' required />
-      </div>
+          {/* user email address */}
+          <label htmlFor="exampleInputEmail" className="form-label">Email address</label>
+          <input type="email" className="form-control text-center fw-bold fs-5" id="exampleInputEmail1" aria-describedby="emailHelp" name='email' required />
+          <div id="emailHelp" className="form-text ">We'll never share your email with anyone else.</div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+          <input type="password" className="form-control text-center fw-bold fs-5" id="exampleInputPassword1" name='password' required />
+        </div>
 
-      <div className="mb-3 form-check">
-        <input type="checkbox" className="form-check-input text-center" id="exampleCheck1" />
-        <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+        <div className="mb-3 form-check">
+          <input type="checkbox" className="form-check-input text-center" id="exampleCheck1" />
+          <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+        </div>
+        <Button type="submit" className="btn btn-primary">Login</Button>
+        <p className='text-danger'>{error}</p>
+        <p>Don't have an account yet? <Link to="/register">Register</Link></p>
+      </Form>
+      {/* google sign in option */}
+      <div className='text-center'>
+        <button className='me-2 btn btn-primary'><FaGoogle></FaGoogle> Login with Google</button>
+        <button className='btn btn-primary'>Login with GitHub</button>
       </div>
-      <Button type="submit" className="btn btn-primary">Login</Button>
-      <p className='text-danger'>{error}</p>
-      <p>Don't have an account yet? <Link to="/register">Register</Link></p>
-    </Form>
-
+    </div>
 
   );
 };
